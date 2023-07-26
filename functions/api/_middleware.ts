617b58fcde418dd4dea6007615b1ede4e2468e5a
Jack7803M <jack@liviewater.com>
2023-07-26 03:04:26 -0400

    turns out cpu timing cannot be logged. error logging still ok

--- functions/api/_middleware.ts
+++ functions/api/_middleware.ts
@@ -1,15 +1,6 @@
 export const onRequest: PagesFunction<unknown> = async (context) => {
     try {
-        // time function execution
-        const start = Date.now();
-
-        const res = await context.next();
-
-        // log function execution time
-        const end = Date.now();
-        console.log(`Function execution time: ${end - start}ms`);
-
-        return res;
+        return await context.next();
     } catch (error) {
         console.error(error);
         return new Response('Internal Server Error', { status: 500 });
